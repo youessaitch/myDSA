968b45416229275b238c426656e7909e268fde8a
youessaitch <72415411+youessaitch@users.noreply.github.com>
2024-06-09 14:01:48 +0530

    Time: 49 ms (26.35%), Space: 36.3 MB (5.08%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
@@ -1,67 +1,37 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        // int n = nums.size();
-        // vector<int> pref(n,0); 
-        // vector<int> prefmod(n,0); 
-        // pref[0] = nums[0];
-        // prefmod[0] = pref[0]%k;
-        // int cnt = 0;
-        // for(int i=1;i<n;i++){
-        //     pref[i] = pref[i-1]+nums[i];
-        //     prefmod[i] = pref[i]%k;
-        //     if(nums[i]%k==0) cnt++;
-        // }
-
-        // unordered_map<int,int> mpp; //prefmod, index
-        // mpp[0]=-1;
-
-        // for(int i=0;i<n;i++){
-        //     if(mpp.find(prefmod[i])!=mpp.end()){
-        //         if(i - mpp[prefmod[i]]>1){
-        //             cnt++;
-        //         }
-        //     }else{
-        //         mpp[prefmod[i]] = i;
-        //     }
-        // }
-
-        // return cnt;
-
         int n = nums.size();
-        vector<int> pref(n,0);
-        vector<int> prefmod(n,0);
-        int cnt = 0;
-        unordered_map<int,int> mpp;
+        vector<int> prefmod(n,0); //store prefix % k
+        int cnt = 0; //to store the ans
+        int pref = 0; // store prefix sum
+        unordered_map<int,int> mpp; //to have a cnt of number of same element in prefmod
+
+        //store prefix sum
         for(int i=0;i<n;i++){
-            if(i==0){ 
-                pref[0]=nums[0];
-                prefmod[0]=(pref[0]%k);
-                if(prefmod[0]<0) prefmod[0]+=k;
-                // cout<<prefmod[0]<<" ";
-                mpp[prefmod[0]]++;
-            }
-            else{
-                pref[i]=pref[i-1]+nums[i];
-                prefmod[i]=(pref[i]%k);
-                if(prefmod[i]<0) prefmod[i]+=k;
-                // cout<<prefmod[i]<<" ";
-                mpp[prefmod[i]]++;
-            }
+            pref += nums[i];
+            prefmod[i]=(pref % k);
+            if(prefmod[i]<0) prefmod[i]+=k; // eg. -2 mod 6 = 4
+            mpp[prefmod[i]]++;
         }
 
+        //iterate the map
         for(auto it: mpp){
-            if(it.second>1){
-                int temp = (it.second-1);
-                cnt += (temp*(temp+1))/2;
+            if(it.second>1){ //a number appears more than once
+                int temp = (it.second-1); 
+                cnt += (temp*(temp+1))/2; //cnt number of distinct pairs that can be formed (simple math)
             }
-            if(it.first==0){
-                cnt+=it.second;
+            if(it.first==0){ //if the prefmod happens to be 0 then the whole subarray from index 0 to the index where prefmod == 0 should be considered as well
+                cnt+=it.second; 
             }
         }
-        return cnt;
+        return cnt; 
 
         //pref[]= 4 9 9 7 4 5
         //mod[] = 4 4 4 2 4 0
+        // map: 4->4   , cnt = 3+2+1 = (it.second-1)*(it.second-1+1)/2;
+        //      2->1    , cnt = 0
+        //      0->1    , cnt = 1;
+        // ans = 6+0+1 = 7;
     }
 };
