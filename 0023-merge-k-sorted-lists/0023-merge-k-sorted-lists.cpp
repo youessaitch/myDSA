569dfd24c2e354ce61ef985a6ce3ebf69fbe5f5b
youessaitch <72415411+youessaitch@users.noreply.github.com>
2024-06-11 13:23:54 +0530

    Time: 8 ms (96.26%), Space: 17.6 MB (21.09%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -9,13 +9,15 @@
  * };
  */
 class Solution {
-public:
+public: 
+    // TC = O(NlogN)
+    // SC = O(N)
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         int n = lists.size();
         priority_queue<int,vector<int>,greater<int>> pq; //min heap(min element on top)
-        for(int i=0;i<n;i++){
-            auto j=lists[i];
-            while(j!=NULL)
+        for(int i=0;i<n;i++){ //O(N*log(N))
+            auto j=lists[i];  
+            while(j!=NULL) //O(1) accessing an element in linkedlist
             {
                 pq.push(j->val);
                 j=j->next;
@@ -23,7 +25,7 @@ public:
         }
         ListNode* ans = new ListNode(-1);
         ListNode* p1 = ans;
-        while(!pq.empty()){
+        while(!pq.empty()){ //O(N*M*log(N*M))
             ListNode* temp = new ListNode(pq.top());
             pq.pop();
             p1->next = temp;
