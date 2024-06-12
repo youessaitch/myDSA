181f19e5c9573aefe28ec620e13964f3e45c8bc7
youessaitch <72415411+youessaitch@users.noreply.github.com>
2024-06-12 13:52:25 +0530

    Time: 7 ms (47.9%), Space: 13.4 MB (23.92%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -16,6 +16,12 @@ public:
 
 class Solution {
 public:
+
+// What does the question asks?
+// I was confused with the question for a bit. But understood it, Basically the question is to deep copy the linked list, that is make an exact copy.(replicate the random nodes too)
+// If it was simple single linked list, it was simple. Just make a copy on the go and connect the nodes.
+// But if the random element is present we have to replicate it to, just imagine you made a new node and the random of this node points to a node which is not yet created ?What will you do? How will you Point to that node, thats what we have to solve here,
+
     Node* copyRandomList(Node* head) {
         Node* p1 = head;
         unordered_map<Node*,Node*> mpp;
