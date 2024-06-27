736f0025fb591704f1d366c94a5382bd36066e94
youessaitch <72415411+youessaitch@users.noreply.github.com>
2024-06-27 14:40:40 +0530

    Time: 77 ms (6.21%), Space: 56.1 MB (52.98%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
@@ -11,31 +11,53 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int> &in){
-        if(!root) return;
+    // void inorder(TreeNode* root, vector<int> &in){
+    //     if(!root) return;
 
-        if(root->left) inorder(root->left,in);
-        in.push_back(root->val);
-        if(root->right) inorder(root->right,in);
-    }
+    //     if(root->left) inorder(root->left,in);
+    //     in.push_back(root->val);
+    //     if(root->right) inorder(root->right,in);
+    // }
 
-    TreeNode* modify(int l, int r, vector<int>& in) {
-        if (l > r) return nullptr;
+    // TreeNode* modify(int l, int r, vector<int>& in) {
+    //     if (l > r) return nullptr;
         
-        int mid = l + (r - l) / 2;
+    //     int mid = l + (r - l) / 2;
         
-        TreeNode* node = new TreeNode(in[mid]);
-        node->left = modify(l, mid - 1, in);
-        node->right = modify(mid + 1, r, in);
-        return node;
-    }
+    //     TreeNode* node = new TreeNode(in[mid]);
+    //     node->left = modify(l, mid - 1, in);
+    //     node->right = modify(mid + 1, r, in);
+    //     return node;
+    // }
+
+    // TreeNode* insertIntoBST(TreeNode* root, int val) {
+    //     if(!root) new TreeNode(val);
+    //     vector<int> in;
+    //     inorder(root,in);
+    //     in.push_back(val);
+    //     sort(in.begin(),in.end());
+    //     return modify(0,in.size()-1,in);
+    // }
 
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        if(!root) new TreeNode(val);
-        vector<int> in;
-        inorder(root,in);
-        in.push_back(val);
-        sort(in.begin(),in.end());
-        return modify(0,in.size()-1,in);
+        if(!root) return new TreeNode(val);
+        TreeNode* p1 = root; //pointer like in linked list
+        while(p1){
+            if(p1->val > val){
+                if(p1->left) p1 = p1->left;
+                else{
+                    p1->left = new TreeNode(val);
+                    break;
+                }
+            }else{
+                if(p1->right) p1 = p1->right;
+                else{
+                    p1->right = new TreeNode(val);
+                    break;
+                }
+            }
+        }
+
+        return root;
     }
 };
