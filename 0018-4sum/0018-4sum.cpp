22ec4511162d52835331abf5c16c44be265fad7d
youessaitch <72415411+youessaitch@users.noreply.github.com>
2024-06-30 22:18:52 +0530

    Time: 335 ms (10.41%), Space: 146 MB (7.71%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-#define ll long long
+    #define ll long long
+
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
         vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
+
+        sort(nums.begin(),nums.end()); //sort 
+
         for(ll i=0;i<n;i++){
-            ll tar = target - nums[i];
+            ll tar = target - nums[i]; //decrease first number from target
             for(ll j=i+1;j<n;j++){
-                ll check = tar - nums[j];
-                ll l = j+1, r = n-1;
+                ll check = tar - nums[j]; //decrease second number from target
+                ll l = j+1, r = n-1; // look for other two numbers
                 while(l<r){
                     ll sum = nums[l]+nums[r];
                     if(sum == check){
                         ans.push_back({nums[i],nums[j],nums[l],nums[r]});
-                        l++;
+                        l++; //if we get one answer look further for more
                         r--;
                     }
-                    else if(sum<check) l++;
+                    else if(sum<check) l++; //simple two pointer
                     else r--;
                 }
             }
         }
-        cout<<ans.size()<<endl;
-        set<vector<int>> temp;
+        // cout<<ans.size()<<endl;
+        set<vector<int>> temp; //avoid duplicates
         for(auto it: ans) temp.insert(it);
         ans.clear();
-        for(auto it: temp) ans.push_back(it);
+        for(auto it: temp) ans.push_back(it); //answer
         return ans;
     }
 };
