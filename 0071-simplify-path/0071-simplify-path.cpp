20557761ed3937f6f48717adfb4fe34f4396b3b4
youessaitch <72415411+youessaitch@users.noreply.github.com>
2024-07-03 19:12:22 +0530

    Time: 7 ms (38.63%), Space: 11.5 MB (36.14%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        int n = path.size();
+        string res = "";
+        stack<string> st;
+        for(int i=0;i<n;i++){
+            if(path[i] == '/') continue;
+            string temp;
+            while(i<n && path[i]!='/'){
+                temp+=path[i];
+                ++i;
+            }
+            if(temp==".") continue;
+            else if(temp==".."){
+                if(!st.empty()) st.pop();
+            }else st.push(temp);
+        }
+
+        while(!st.empty()){
+            res = "/" + st.top() + res;
+            st.pop();
+        }
+
+        if(res.size()==0) return "/";
+        return res;
+    }
+};
