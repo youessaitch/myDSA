6e8e6cafc7ab3c70b4952f092fca53cee4c053bb
youessaitch <72415411+youessaitch@users.noreply.github.com>
2024-07-05 10:07:07 +0530

    Time: 193 ms (7.63%), Space: 130.6 MB (5.36%) - LeetHub

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -14,25 +14,25 @@ public:
         vector<int> v;
         ListNode* p1 = head;
         while(p1!=nullptr){
-            v.push_back(p1->val);
+            v.push_back(p1->val); //store the values in a vector
             p1 = p1->next;
         }
         int n = v.size();
-        if(n<=3) return {-1,-1};
+        if(n<=3) return {-1,-1}; //if n<=3, there cant be two critical points
 
         vector<int> critical;
-        for(int i=1;i<n-1;i++){
+        for(int i=1;i<n-1;i++){ //push the indices of all the critical points
             if((v[i]>v[i-1] && v[i]>v[i+1]) || (v[i]<v[i-1] && v[i]<v[i+1])){
                 critical.push_back(i);
             }
         }
-        if(critical.size()<2) return {-1,-1};
+        if(critical.size()<2) return {-1,-1}; //there cant be two critical points
         int maxi = -1e5;
         int mini = 1e5;
         sort(critical.begin(),critical.end());
-        maxi = critical[critical.size()-1] - critical[0];
+        maxi = critical[critical.size()-1] - critical[0]; //maximum diff
         for(int i=1;i<critical.size();i++){
-            mini = min(mini,critical[i]-critical[i-1]);
+            mini = min(mini,critical[i]-critical[i-1]); //finding minimum
         }
         return {mini,maxi};
     }
