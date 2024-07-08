38e4df236f9a6caeb485f064fd9826a000f90221
youessaitch <72415411+youessaitch@users.noreply.github.com>
2024-07-08 12:58:05 +0530

    Time: 0 ms (100%), Space: 7.1 MB (76.96%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        vector<int> v(n,1);
-        int t = n;
-        int i = 0;
-        int K = k;
-        while(t>1){
-            while(K!=1){
-                if(v[i%n]==-1){
-                    i++;
-                    continue;
-                }
-                i++;
-                K--;
-            }
-            while(v[i%n]==-1) i++;
+        // vector<int> v(n,1);
+        // int t = n;
+        // int i = 0;
+        // int K = k;
+        // while(t>1){ //n
+        //     while(K!=1){
+        //         if(v[i%n]==-1){
+        //             i++;
+        //             continue;
+        //         }
+        //         i++;
+        //         K--;
+        //     }
+        //     while(v[i%n]==-1) i++;
             
-            if(K==1){
-                t--;
-                v[i%n]=-1;
-                i++;
-                K=k;
-            }
-        }
+        //     if(K==1){
+        //         t--;
+        //         v[i%n]=-1;
+        //         i++;
+        //         K=k;
+        //     }
+        // }
 
-        for(int i=0;i<n;i++){
-            if(v[i]==1) return i+1;
+        // for(int i=0;i<n;i++){
+        //     if(v[i]==1) return i+1;
+        // }
+        // return -1;
+        int ans = 1;
+        for(int i=2;i<=n;i++){
+            ans = (ans + (k-1))%i+1;
         }
-        return -1;
+        return ans;
     }
 };
