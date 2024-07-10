937a541380ef378b8828ab918de52cec4f02fe1a
youessaitch <72415411+youessaitch@users.noreply.github.com>
2024-07-10 12:53:44 +0530

    Time: 22 ms (43.58%), Space: 19.7 MB (63.2%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int n,m;
+    vector<vector<int>> dp; 
+
+    // dp[i][j] = string upto ith index in s is common to jth index in t
+
+    int solve(int i, int j, string &s, string &t){
+        if(j==m) return 1;
+        if(i==n) return 0;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int take = 0;
+        if(s[i]==t[j]){
+            take = solve(i+1,j+1,s,t);
+        }
+
+        int notTake = solve(i+1,j,s,t);
+
+        return dp[i][j] = take + notTake;
+    }
+
+    int numDistinct(string s, string t) {
+        n = s.size();
+        m = t.size();
+        dp.resize(n+1,vector<int>(m+1,-1));
+        return solve(0,0,s,t);
+    }
+};
