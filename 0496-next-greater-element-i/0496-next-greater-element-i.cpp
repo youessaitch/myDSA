e1a02b0ff9c533bdc1aaca1dd131e75ab5eb0b0c
youessaitch <72415411+youessaitch@users.noreply.github.com>
2024-07-14 13:42:13 +0530

    Time: 8 ms (31.57%), Space: 12.6 MB (5.11%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,50 +1,24 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        vector<int> nge(m);
+        //2 3 -1 -1
+        int n = nums2.size();
+        vector<int> v(n,-1);
         stack<int> st;
-        for(int i=m-1;i>=0;i--){
-            if(st.empty()){
-                nge[i]=-1;
-                st.push(nums2[i]);
-                continue;
-            }
-            if(!st.empty()){
-                if(st.top()>nums2[i]){
-                    nge[i]=st.top();
-                    st.push(nums2[i]);
-                }else{
-                    bool emp = 0;
-                    while(nums2[i]>st.top()){
-                        st.pop();
-                        if(st.empty()){
-                            emp =1;
-                            break;
-                        }
-                    }
-
-                    if(emp){
-                        nge[i] = -1;
-                        st.push(nums2[i]);
-                    }else{
-                        nge[i] = st.top();
-                        st.push(nums2[i]);
-                    }
-
-                }
-            }
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && st.top()<=nums2[i]) st.pop();
+            if(!st.empty() && st.top()>nums2[i]) v[i] = st.top();
+            st.push(nums2[i]); 
         }
 
         unordered_map<int,int> mpp;
-        for(int i=0;i<m;i++){
-            mpp[nums2[i]] = nge[i];
+        for(int i=0;i<n;i++){
+            mpp[nums2[i]] = v[i];
         }
 
-        vector<int> ans(n);
-        for(int i=0;i<n;i++){
-            ans[i] = mpp[nums1[i]];
+        vector<int> ans;
+        for(auto it: nums1){
+            ans.push_back(mpp[it]);
         }
         return ans;
     }
