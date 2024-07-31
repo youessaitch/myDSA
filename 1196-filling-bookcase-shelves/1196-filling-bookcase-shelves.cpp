8b3037a9331afb9c29ecf7de335d111ff3e39785
youessaitch <72415411+youessaitch@users.noreply.github.com>
2024-07-31 16:16:28 +0530

    Time: 4 ms (60.3%), Space: 21 MB (14.03%) - LeetHub

--- 1196-filling-bookcase-shelves/1196-filling-bookcase-shelves.cpp
+++ 1196-filling-bookcase-shelves/1196-filling-bookcase-shelves.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     int n; 
-    vector<vector<int>> dp;
+    vector<vector<int>> dp; //{index,remainingWeight in shelf}
     int solve(int i, int remainW, vector<vector<int>> &books, int maxW, int &shelfWidth){
         if(i>=n) return maxW;
 
         if(dp[i][remainW]!=-1) return dp[i][remainW];
 
-        int take = INT_MAX;
-        if(books[i][0]<=remainW){
-            take = solve(i+1,remainW-books[i][0],books,max(maxW,books[i][1]),shelfWidth);
+        int sameShelf = INT_MAX;
+        if(books[i][0] <= remainW){ //place in same layer of shelf
+            sameShelf = solve(i+1,remainW-books[i][0],books,max(maxW,books[i][1]),shelfWidth);
         }
-        int notTake = maxW + solve(i+1,shelfWidth-books[i][0],books,books[i][1],shelfWidth);
-        return dp[i][remainW] = min(take,notTake);
+        //when moving to new layer of shelf, add maxW from previous layer and proceed
+        int newShelf = maxW + solve(i+1,shelfWidth-books[i][0],books,books[i][1],shelfWidth);
+
+        //return minimum
+        return dp[i][remainW] = min(sameShelf,newShelf);
     }
 
     int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
         n = books.size();
-        //{thickness,height}
+        //books -> {thickness,height}
         dp.resize(n,vector<int>(shelfWidth+1,-1));
         int remainW = shelfWidth;
         return solve(0,remainW,books,0,shelfWidth);
