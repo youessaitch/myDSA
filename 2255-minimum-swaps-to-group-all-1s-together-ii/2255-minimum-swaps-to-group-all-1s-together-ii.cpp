9ec478310b9aa5d50489eaa8e7f0e233228310a8
youessaitch <72415411+youessaitch@users.noreply.github.com>
2024-08-02 12:21:21 +0530

    Time: 74 ms (31.83%), Space: 100.3 MB (5.11%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -3,16 +3,20 @@ public:
     int minSwaps(vector<int>& nums) {
         int n = nums.size();
         int ones = 0;
+
+        //create a new extended array (nums+nums)
         vector<int> newNums;
         newNums = nums;
         newNums.insert(newNums.end(),nums.begin(),nums.end());
         int m = newNums.size();
-        vector<int> pref(m,0); //prefix for 0
+    
+        vector<int> pref(m,0); //prefix for '0'
 
-        for(auto it: nums){
+        for(auto it: nums){ //cnt 1's -> sliding window size
             if(it==1) ones++;
         }
         
+        //calculate number of zeroes upto each index
         for(int i=0;i<m;i++){
             if(i==0){
                 if(newNums[i]==0) pref[i]=1;
@@ -22,15 +26,17 @@ public:
             }
         }
 
-        if(ones==0) return 0;
+        if(ones==0) return 0; //if no ones return 0
 
-        for(int i=0;i<n;i++){
-            cout<<pref[i]<<" ";
-        }
+        // for(int i=0;i<n;i++){
+        //     cout<<pref[i]<<" ";
+        // }
 
         int l=0;
         int r=ones-1;        
         int ans = 1e9;
+
+        //sliding window
         while(r<m){
             int zero;
             if(l==0) zero = pref[r];
