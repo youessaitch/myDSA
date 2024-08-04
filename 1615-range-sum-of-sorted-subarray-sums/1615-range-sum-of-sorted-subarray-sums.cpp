e4ba1b845ec221cdb6e337b33ce8d3f4633679ec
youessaitch <72415411+youessaitch@users.noreply.github.com>
2024-08-04 23:47:02 +0530

    Time: 87 ms (73.71%), Space: 27 MB (44.27%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
@@ -2,33 +2,37 @@ class Solution {
 public:
     int m = 1e9+7;
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        multiset<int> st;
+        // multiset<int> st;
+        vector<int> v;
         for(int i=0;i<n;i++){
             int sum = 0;
             for(int j=i;j<n;j++){
                 sum+=nums[j];
-                st.insert(sum);
+                // st.insert(sum);
+                v.push_back(sum);
             }
         }
+        sort(v.begin(),v.end());
         int i=0;
         bool ch = false;
         int ans = 0;
-        for(auto it: st){
-            if(i==left-1){
-                cout<<"left: "<< i<<endl;
-                ch = true;
-            }
+        // for(auto it: st){
+        //     if(i==left-1){
+        //         cout<<"left: "<< i<<endl;
+        //         ch = true;
+        //     }
 
-            if(ch){
-                ans = (ans+it)%m;
-                // ans=ans%m; 
-            }
-            if(i==right-1){ 
-                cout<<"left: "<< i<<endl;
-                break;
-            }
-            i++;
-        }
+        //     if(ch){
+        //         ans = (ans+it)%m;
+        //         // ans=ans%m; 
+        //     }
+        //     if(i==right-1){ 
+        //         cout<<"left: "<< i<<endl;
+        //         break;
+        //     }
+        //     i++;
+        // }
+        for(int i=left-1;i<=right-1;i++) ans = (ans+v[i])%m;
         return ans;
     }
 };
