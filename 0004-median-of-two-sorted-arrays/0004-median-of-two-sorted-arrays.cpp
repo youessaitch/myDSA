80f7607a019e8ad061a65726a7e972d7529ff95f
youessaitch <72415411+youessaitch@users.noreply.github.com>
2024-08-12 19:10:58 +0530

    Time: 15 ms (94.65%), Space: 94.8 MB (47.25%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,6 +4,7 @@ public:
         int n = nums1.size();
         int m = nums2.size();
 
+        // if one of the array is empty simply return median
         if(n==0){
             if(m%2==1) return 1.0*nums2[m/2];
             else return 1.0*(nums2[m/2]+nums2[m/2-1])/2.0;
@@ -14,16 +15,19 @@ public:
             else return 1.0*(nums1[n/2]+nums1[n/2-1])/2.0;
         }
 
+        // if (n>m) swap the two arrays
         if (n > m) {
             return findMedianSortedArrays(nums2, nums1);
         }
 
+        //binary search to find lengths of partitions
         int lo=0, hi=n;
         while(lo<=hi){
-            int mid1 = (lo+hi)/2;
-            int mid2 = (n+m+1)/2 - mid1;
+            int mid1 = (lo+hi)/2; //partition 1
+            int mid2 = (n+m+1)/2 - mid1; //partition 2
 
-            int l1=INT_MIN, l2=INT_MIN;
+            //if l1,l2,r1,r2 doesnt get updated then still we should have l1<=r1 && l2<=r1
+            int l1=INT_MIN, l2=INT_MIN; 
             int r1=INT_MAX, r2=INT_MAX;
 
             if(mid1<n) r1 = nums1[mid1];
@@ -32,9 +36,11 @@ public:
             if(mid2-1>=0) l2 = nums2[mid2-1];
 
             if(l1<=r2 && l2<=r1){
-                if((n+m)%2==0)
+                if((n+m)%2==0){ // even number of elements
                     return 1.0*(max(l1,l2) + min(r1,r2))/2.0;
-                else return 1.0*max(l1,l2);
+                }
+                //if odd just return max of (l1,l2)
+                else return 1.0*max(l1,l2); 
             }else if(l1 > r2) hi=mid1-1;
             else lo = mid1+1;
         }
