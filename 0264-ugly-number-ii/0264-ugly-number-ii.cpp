87e3f393e07c40a88e6b194cca5d9e8664a869b6
youessaitch <72415411+youessaitch@users.noreply.github.com>
2024-08-18 16:13:26 +0530

    Time: 0 ms (100%), Space: 9.7 MB (72.38%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -0,0 +1,30 @@
+#define ll long long
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> v(n+1);
+        //v[i] = ith ugly number, return v[n];
+        //v[0] = not of any use
+
+        //pointing to first ugly number
+        int i2=1,i3=1,i5=1;
+
+        //1st ugly number is 1
+        v[1]=1;
+
+        for(int i=2;i<=n;i++){
+            int t2 = v[i2]*2;
+            int t3 = v[i3]*3;
+            int t5 = v[i5]*5;
+
+            int mini = min(t2,min(t3,t5));
+
+            v[i] = mini;
+            if(mini==t2) i2++;
+            if(mini==t3) i3++;
+            if(mini==t5) i5++;
+        }
+
+        return v[n];
+    }
+};
