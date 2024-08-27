3770770f76df948b4d40dbd941f7e6c0e0516396
Ayush <72415411+youessaitch@users.noreply.github.com>
2024-08-27 18:51:29 +0530

    Time: 28 ms (72.05%), Space: 35.1 MB (49.28%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,56 +1,52 @@
 class Solution {
 public:
+    int n;
+    
+    int findFloor(vector<int> &arr, int x) {
+        int i = 0, j = n - 1;
+        int ans = -1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (arr[mid] <= x) {
+                ans = mid;
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+        }
+        return ans;
+    }
+
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int n = arr.size();
+        n = arr.size();
         vector<int> ans;
-        vector<int> v(n);
-        int mini = INT_MAX;
-        int idx = -1;
-
-        for(int i=0;i<n;i++){
-            v[i] = abs(arr[i]-x);
-            if(v[i]<mini){
-                mini = v[i];
-                idx = i;
+        
+        // Find the floor index
+        int idx = findFloor(arr, x);
+        
+        // Initialize pointers around idx
+        int i = idx;
+        int j = idx + 1;
+        
+        // Expand outwards from the closest element
+        while (k > 0) {
+            if (i >= 0 && j < n) {
+                if (x - arr[i] <= arr[j] - x) {
+                    ans.push_back(arr[i--]);
+                } else {
+                    ans.push_back(arr[j++]);
+                }
+            } else if (i >= 0) {
+                ans.push_back(arr[i--]);
+            } else if (j < n) {
+                ans.push_back(arr[j++]);
             }
+            k--;
         }
-        ans.push_back(arr[idx]);
-
-        k--;
-        int i=idx-1;
-        int j=idx+1;
-        while(k>0){
-            if(i>=0 && j<n){
-                if(v[i]<v[j]){
-                    ans.push_back(arr[i]);
-                    i--;
-                    k--;
-                }else if(v[i]>v[j]){
-                    ans.push_back(arr[j]);
-                    j++;
-                    k--;
-                } else{
-                    ans.push_back(arr[i]);
-                    i--;
-                    k--;
-                }
-            }
-
-            else if(i>=0 && j>=n){
-                for(int p=i;p>=0;p--){
-                    ans.push_back(arr[p]);
-                    k--;
-                    if(k==0) break;
-                }
-            }else if(i<0 && j<n){
-                for(int p=j;p<n;p++){
-                    ans.push_back(arr[p]);
-                    k--;
-                    if(k==0) break;
-                }
-            }
-        }
-        sort(ans.begin(),ans.end());
+        
+        // Sort the result to maintain the order
+        sort(ans.begin(), ans.end());
+        
         return ans;
     }
 };
