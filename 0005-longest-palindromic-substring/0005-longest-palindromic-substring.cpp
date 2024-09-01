b3ca98d65f7fc9c89f1f9b0ca880a13688026a32
Ayush <72415411+youessaitch@users.noreply.github.com>
2024-09-01 18:54:58 +0530

    Time: 19 ms (66.77%), Space: 22.1 MB (51.34%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,34 +1,57 @@
 class Solution {
 public:
     int n;
-    vector<vector<int>> dp;
+    // vector<vector<int>> dp;
 
-    bool solve(int i, int j, string &s){
-        if(i>=j) return true;
+    // bool solve(int i, int j, string &s){
+    //     if(i>=j) return true;
 
-        if(dp[i][j]!=-1) return dp[i][j];
+    //     if(dp[i][j]!=-1) return dp[i][j];
 
-        if(s[i]==s[j]){
-            return dp[i][j] = solve(i+1,j-1,s);
-        }
+    //     if(s[i]==s[j]){
+    //         return dp[i][j] = solve(i+1,j-1,s);
+    //     }
 
-        return dp[i][j] = false;
-    }
+    //     return dp[i][j] = false;
+    // }
 
     string longestPalindrome(string s) {
         n = s.size();
 
         string ans = ""; //ans
         int maxi = 0; //size
-        dp.resize(n,vector<int>(n,-1));
+        // dp.resize(n,vector<int>(n,-1));
+        // for(int i=0;i<n;i++){
+        //     for(int j=i;j<n;j++){
+        //         if(solve(i,j,s)){
+        //             if((j-i+1) > maxi){
+        //                 maxi = (j-i+1);
+        //                 ans = s.substr(i,maxi);
+        //             }
+        //         }
+        //     }
+        // }
+
         for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(solve(i,j,s)){
-                    if((j-i+1) > maxi){
-                        maxi = (j-i+1);
-                        ans = s.substr(i,maxi);
-                    }
+            int l=i,r=i;
+
+            while(l>=0 && r<=n && s[l]==s[r]){
+                if(r-l+1 > maxi){
+                    maxi = r-l+1;
+                    ans = s.substr(l,maxi);
                 }
+                l--;
+                r++;
+            }
+
+            l=i,r=i+1;
+            while(l>=0 && r<=n && s[l]==s[r]){
+                if(r-l+1 > maxi){
+                    maxi = r-l+1;
+                    ans = s.substr(l,maxi);
+                }
+                l--;
+                r++;
             }
         }
 
