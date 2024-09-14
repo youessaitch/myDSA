338499cc1b9bf98c70a0632bed2709be802264c9
Ayush <72415411+youessaitch@users.noreply.github.com>
2024-09-14 15:48:46 +0530

    Time: 11 ms (97.19%), Space: 24.8 MB (97.09%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    //Note: before the required element to be found, the index of elements at the first occurrence will be even and after that required element, the index of first occurences will be odd
+    //e.g. 1 1 2 3 3 4 4 8 8
+    //idx  0 1 2 3 4 5 6 7 8
+    //2 is the required element
+    //before 2 first occurences exist at index: 0 (even)
+    //after 2 first occurences exist at index: 3,5,7(odd)
     int n;
     int singleNonDuplicate(vector<int>& nums) {
         n = nums.size();
