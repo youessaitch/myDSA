5f7b5c047809560af41eab4d6f59c3b4a60badb5
Ayush <72415411+youessaitch@users.noreply.github.com>
2024-09-24 02:07:52 +0530

    Time: 27 ms (33.31%), Space: 22.9 MB (72.73%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& v) {
-        int n = v.size();
-        sort(v.begin(),v.end());
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(),intervals.end());
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
         vector<vector<int>> ans;
-        stack<pair<int,int>> st;
+
         for(int i=0;i<n;i++){
-            if(!st.empty()){
-                auto t = st.top();
-                if((v[i][0] >= t.first) && (v[i][0] <= t.second)){
-                    st.pop();
-                    st.push({min(v[i][0],t.first),max(v[i][1],t.second)});
-                }else{
-                    st.push({v[i][0],v[i][1]});
-                }
-            }else{
-                st.push({v[i][0],v[i][1]});
+            // if(i<n-1 && intervals[i+1][0]>intervals[i][1]){ 
+                
+            // }
+            if(i<n-1 && end>=intervals[i+1][0]){
+                end = max(end,intervals[i+1][1]);
+            }else if(i<n-1 && end<intervals[i+1][0]){
+                ans.push_back({start,end});
+                start = intervals[i+1][0];
+                end = intervals[i+1][1];
             }
-            
         }
-        while(!st.empty()){
-            ans.push_back({st.top().first, st.top().second});
-            st.pop();
-        }
-        reverse(ans.begin(),ans.end());
+
+        if(start!=-1 && end!=0)
+            ans.push_back({start,end});
+
         return ans;
     }
 };
