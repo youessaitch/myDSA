ca5703674c4260da2d382a07861c4dd4fa5c9dea
Ayush <72415411+youessaitch@users.noreply.github.com>
2024-09-25 14:42:25 +0530

    Time: 604 ms (47.38%), Space: 704.2 MB (63.84%) - LeetHub

--- 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.cpp
+++ 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.cpp
@@ -1,33 +1,63 @@
-struct Trie{
-    int score;
+struct Trie {
+    int score = 0;
     Trie* children[26];
-    Trie(){
-        score = 0;
-        memset(children,0,sizeof(children));
-    }
-
-    //insert a word into trie
-    void add(string &s, int i){
-        if(i) score++;
-        if(i == s.size()) return;
-        if(!children[s[i]-'a']) children[s[i]-'a'] = new Trie();
-        children[s[i]-'a']->add(s,i+1);
-    }
-
-    int dfs(string &s, int i){
-        if(i==s.size()) return score;
-        return score + children[s[i]-'a']->dfs(s,i+1);
-    }
 };
 
 class Solution {
 public:
+
+    Trie* getTrie() {
+        Trie* newNode = new Trie();
+        for(int i = 0; i < 26; i++) {
+            newNode->children[i] = nullptr;
+        }
+        newNode->score = 0;
+        return newNode;
+    }
+
+    void insert(string& word, Trie* root) {
+        Trie* p = root;
+
+        for(char &ch : word) {
+            int idx = ch - 'a';
+
+            if(!p->children[idx]) {
+                p->children[idx] = getTrie();
+            }
+
+            p->children[idx]->score += 1;
+            p = p->children[idx];
+        }
+    }
+
+    int getScore(string& word, Trie* root) {
+        Trie* p = root; //"abc"
+        int score = 0;
+
+        for(char &ch : word) {
+            int idx = ch - 'a';
+            score += p->children[idx]->score;
+
+            p = p->children[idx]; // now i am at "b" wala node
+        }
+
+        return score;
+    }
+
     vector<int> sumPrefixScores(vector<string>& words) {
         int n = words.size();
-        vector<int> ans;
-        Trie* trie = new Trie();
-        for(auto it: words) trie->add(it,0);
-        for(auto it: words) ans.push_back(trie->dfs(it,0));
-        return ans;
+
+        Trie* root = getTrie();
+        for(string &word : words) {
+            insert(word, root);
+        }
+
+        vector<int> result(n);
+        //result[i] = sum of scores of prefixes of words[i]
+        for(int i = 0; i < n; i++) {
+            result[i] = getScore(words[i], root);
+        }
+
+        return result;
     }
 };
