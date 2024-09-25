49d55e625912e23eefa8545064f4a894a10cdde0
Ayush <72415411+youessaitch@users.noreply.github.com>
2024-09-25 20:28:51 +0530

    Time: 45 ms (70.39%), Space: 48.6 MB (27.52%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,68 +1,45 @@
-// Node structure for Trie
-struct Node {
-    Node* links[26]; // Array of pointers to child nodes for each alphabet
-    bool flag = false; // Flag to mark end of a word
-
-    // Function to check if a key exists in the children nodes
-    bool containsKey(char ch) { return (links[ch - 'a'] != NULL); }
-
-    // Function to insert a key and corresponding node in children nodes
-    void put(char ch, Node* node) { links[ch - 'a'] = node; }
-
-    // Function to get the child node corresponding to a key
-    Node* get(char ch) { return links[ch - 'a']; }
-
-    // Function to mark the current node as the end of a word
-    void setEnd() { flag = true; }
-
-    // Function to check if the current node marks the end of a word
-    bool isEnd() { return flag; }
+class TrieNode{
+public:
+    TrieNode *child[26];
+    bool isWord;
+    TrieNode(){
+        isWord = false;
+        for(int i=0;i<26;i++){
+            child[i] = nullptr;
+        }
+    }
 };
 
-// Trie class for string operations
 class Trie {
-private: 
-    Node* root; // Root node of the Trie
+    TrieNode* root;
 public:
-    // Constructor to initialize the Trie with an empty root node
     Trie() {
-        root = new Node();
+        root = new TrieNode();
     }
     
-    // Function to insert a word into the Trie
     void insert(string word) {
-        Node* node = root; // Start from the root
-        for(auto ch: word){
-            if(!node->containsKey(ch)){ // If the current node doesn't have the key
-                node->put(ch,new Node()); // Create a new node and link it
-            }
-            node = node->get(ch); // Move to the child node
+        TrieNode* p = root;
+        for(auto it: word){
+            int i = it-'a';
+            if(!p->child[i]) p->child[i] = new TrieNode();
+            p = p->child[i];
         }
-        node->setEnd(); // Mark the last node as the end of a word
+        p->isWord = true;
     }
     
-    // Function to search for a word in the Trie
-    bool search(string word) {
-        Node* node = root; // Start from the root
-        for(auto ch: word){
-            if(!node->containsKey(ch)){ // If the key doesn't exist in the children nodes
-                return false; // Word not found
-            }
-            node = node->get(ch); // Move to the child node
+    bool search(string word, bool prefix = false) {
+        TrieNode* p = root;
+        for(auto it: word){
+            int i = it-'a';
+            if(!p->child[i]) return false;
+            p = p->child[i];
         }
-        return node->isEnd(); // Check if the last node marks the end of a word
+        if(!prefix) return p->isWord;
+        return true;
     }
     
-    // Function to check if there is any word in the Trie that starts with a given prefix
     bool startsWith(string prefix) {
-        Node* node = root; // Start from the root
-        for(auto ch: prefix){
-            if(!node->containsKey(ch)){ // If the key doesn't exist in the children nodes
-                return false; // Prefix not found
-            }
-            node = node->get(ch); // Move to the child node
-        }
-        return true; // Prefix found
+        return search(prefix,true);
     }
 };
 
