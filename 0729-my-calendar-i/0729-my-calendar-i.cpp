a6ae70fbc444333bc89ce2310c30c2e58d47aba1
Ayush <72415411+youessaitch@users.noreply.github.com>
2024-09-27 18:07:33 +0530

    Time: 81 ms (55.86%), Space: 41.6 MB (86.63%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -4,25 +4,17 @@ public:
     MyCalendar() {}
 
     bool book(int start, int end) {
-        int l = 0;
-        int r = intervals.size()-1;
+       
+        for(auto it: intervals){
+            int pehla = it.first;
+            int dusra = it.second;
 
-        while(l<=r){
-            int mid = l + (r-l)/2;
-
-            int s = intervals[mid].first;
-            int e = intervals[mid].second;
-
-            if(s<end && e>start) return false;
-
-            if(start >= e){
-                l = mid+1;
-            }else if(end <= s){
-                r = mid-1;
+            if(max(pehla,start) < min(dusra,end)){ //overlapping
+                return false;
             }
         }
 
-        intervals.insert(intervals.begin()+l,{start,end}); // No overlap, add interval
+        intervals.push_back({start,end});
         return true;
     }
 };
@@ -32,3 +24,25 @@ public:
  * MyCalendar* obj = new MyCalendar();
  * bool param_1 = obj->book(start,end);
  */
+
+
+  // int l = 0;
+        // int r = intervals.size()-1;
+
+        // while(l<=r){
+        //     int mid = l + (r-l)/2;
+
+        //     int s = intervals[mid].first;
+        //     int e = intervals[mid].second;
+
+        //     if(s<end && e>start) return false;
+
+        //     if(start >= e){
+        //         l = mid+1;
+        //     }else if(end <= s){
+        //         r = mid-1;
+        //     }
+        // }
+
+        // intervals.insert(intervals.begin()+l,{start,end}); // No overlap, add interval
+        // return true;
