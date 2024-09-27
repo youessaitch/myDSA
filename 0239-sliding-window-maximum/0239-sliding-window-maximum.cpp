48aa84a431e1c25693c92cf1c5e09319e6f3696b
Ayush <72415411+youessaitch@users.noreply.github.com>
2024-09-27 18:20:14 +0530

    Time: 488 ms (11.13%), Space: 179.5 MB (10.02%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -3,17 +3,16 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> ans;
         int n = nums.size();
-        // multiset<int> ms;
-        map<int,int> ms;
+        map<int,int, greater<int>> ms;
         for(int i=0;i<k;i++) ms[nums[i]]++;
-        // auto it = *ms.rbegin();
-        // ans.push_back(it);
-        ans.push_back(ms.rbegin()->first);
-        for(int i=k;i<n;i++){
-           ms[nums[i-k]]--;
-           if(ms[nums[i-k]]==0) ms.erase(nums[i-k]);
-           ms[nums[i]]++;
-           ans.push_back(ms.rbegin()->first);
+        ans.push_back(ms.begin()->first);
+        int i=0;
+        for(int j=k;j<n;j++){
+           ms[nums[i]]--;
+           if(ms[nums[i]]==0) ms.erase(nums[i]);
+           ms[nums[j]]++;
+           i++;
+           ans.push_back(ms.begin()->first);
         }
         return ans;
     }
