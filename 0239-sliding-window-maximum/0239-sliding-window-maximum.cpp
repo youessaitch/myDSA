8695dd4298667d559ca4431b41a93b9418f79094
Ayush <72415411+youessaitch@users.noreply.github.com>
2024-09-27 18:57:06 +0530

    Time: 180 ms (38.66%), Space: 137.5 MB (34.69%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        // map was nlogn
+        // deque will be O(n)
+        // monotonic deque
+        deque<int> dq;
         vector<int> ans;
-        int n = nums.size();
-        map<int,int, greater<int>> ms;
-        for(int i=0;i<k;i++) ms[nums[i]]++;
-        ans.push_back(ms.begin()->first);
-        int i=0;
-        for(int j=k;j<n;j++){
-           ms[nums[i]]--;
-           if(ms[nums[i]]==0) ms.erase(nums[i]);
-           ms[nums[j]]++;
-           i++;
-           ans.push_back(ms.begin()->first);
+
+        for(int i=0;i<k;i++){
+            while(!dq.empty() && nums[i]>=nums[dq.back()]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
         }
+
+        ans.push_back(nums[dq.front()]);
+
+        for(int i=k;i<nums.size();i++){
+            //out of bound remove
+            while(!dq.empty() && dq.front() <= i-k){
+                dq.pop_front();
+            }
+
+            while(!dq.empty() && nums[i]>=nums[dq.back()]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+
+            ans.push_back(nums[dq.front()]);
+        }
+
         return ans;
     }
 };
