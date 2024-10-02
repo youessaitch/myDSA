3bfe2c4d30851b955e97f77d725ef6286acaa9eb
Ayush <72415411+youessaitch@users.noreply.github.com>
2024-10-02 13:09:21 +0530

    Time: 152 ms (21.56%), Space: 65.5 MB (24.42%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,34 +1,42 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        vector<int> ans;
         int n = graph.size();
-        vector<int> Revadj[n]; //stores linked list
-        //we need to work with outdegree, so lets just make reverse list, that way we can work with indegree ( since reversed)
 
-        //Note : if adj is x[0]->x[1], we write indegree for x[1] and not x[0], so indegree[x[1]]++;
-        vector<int> indegree(n, 0);
+        vector<vector<int>> revAdj(n);
         for(int i=0;i<n;i++){
-            for(auto it: graph[i]){
-                Revadj[it].push_back(i);
-                indegree[i]++;
+            for(int j=0;j<graph[i].size();j++){
+                revAdj[graph[i][j]].push_back(i);
+            }
+        }
+
+        vector<int> indegree(n,0); //outdegree for original graph
+
+        for(int i=0;i<n;i++){
+            for(auto it: revAdj[i]){
+                indegree[it]++;
             }
         }
 
         queue<int> q;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<indegree.size();i++){
             if(indegree[i]==0) q.push(i);
         }
+
+        vector<int> ans;
 
         while(!q.empty()){
             auto node = q.front();
             q.pop();
+
             ans.push_back(node);
-            for(auto it: Revadj[node]){
+
+            for(auto it: revAdj[node]){
                 indegree[it]--;
                 if(indegree[it]==0) q.push(it);
             }
         }
+
         sort(ans.begin(),ans.end());
         return ans;
     }
