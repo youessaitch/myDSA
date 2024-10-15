70532d10bed3d9150db317130e906677c6e431ff
Ayush <72415411+youessaitch@users.noreply.github.com>
2024-10-15 14:04:45 +0530

    Time: 31 ms (73.47%), Space: 15.6 MB (45.68%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -6,12 +6,23 @@ public:
     long long minimumSteps(string s) {
         n = s.size();
         ll ans = 0;
-        
-        ll zero = 0;
+
+        ll zeroPos=-1;
 
         for(int i=n-1;i>=0;i--){
-            if(s[i] == '0') zero++;
-            else ans += zero;
+            if(s[i] == '0'){
+                zeroPos = i;
+                break;
+            }
+        }
+
+        if(zeroPos == -1) return 0;
+
+        for(int i = zeroPos;i>=0;i--){
+            if(s[i] == '1'){
+                ans += (zeroPos - i);
+                zeroPos--;
+            }
         }
 
         return ans;
