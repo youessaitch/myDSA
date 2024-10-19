00c0060f32ebd26526523bd6b8c5d02adeab69b0
Ayush <72415411+youessaitch@users.noreply.github.com>
2024-10-19 14:03:46 +0530

    Time: 423 ms (12.81%), Space: 214.7 MB (5.88%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,37 +1,61 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        // map was nlogn
-        // deque will be O(n)
-        // monotonic deque
-        deque<int> dq;
+        int n = nums.size();
         vector<int> ans;
 
-        for(int i=0;i<k;i++){
-            //aage wale chote hai not needed so remove ( monotonic )
-            while(!dq.empty() && nums[i]>=nums[dq.back()]){
-                dq.pop_back();
-            }
-            dq.push_back(i);
-        }
+        multimap<int,int> mpp;
 
-        ans.push_back(nums[dq.front()]);
+        int i=0,j=0;
 
-        for(int i=k;i<nums.size();i++){
-            //out of bound remove
-            while(!dq.empty() && dq.front() <= i-k){
-                dq.pop_front();
-            }
-            
-            //aage wale chote hai not needed so remove ( monotonic )
-            while(!dq.empty() && nums[i]>=nums[dq.back()]){
-                dq.pop_back();
-            }
-            dq.push_back(i);
+        while(j<n){
+            mpp.insert({nums[j],1});
 
-            ans.push_back(nums[dq.front()]);
+            if(j-i+1 > k){
+                if(mpp.find(nums[i])!=mpp.end()) mpp.erase(mpp.find(nums[i])); //delete only one occurence
+                i++;
+            }
+
+            if(j-i+1 == k){
+                ans.push_back(mpp.rbegin()->first);
+            }
+
+            j++;
         }
 
         return ans;
-    }
+    }   
 };
+
+//          // map was nlogn
+//         // deque will be O(n)
+//         // monotonic deque
+//         deque<int> dq;
+//         vector<int> ans;
+
+//         for(int i=0;i<k;i++){
+//             //aage wale chote hai not needed so remove ( monotonic )
+//             while(!dq.empty() && nums[i]>=nums[dq.back()]){
+//                 dq.pop_back();
+//             }
+//             dq.push_back(i);
+//         }
+
+//         ans.push_back(nums[dq.front()]);
+
+//         for(int i=k;i<nums.size();i++){
+//             //out of bound remove
+//             while(!dq.empty() && dq.front() <= i-k){
+//                 dq.pop_front();
+//             }
+            
+//             //aage wale chote hai not needed so remove ( monotonic )
+//             while(!dq.empty() && nums[i]>=nums[dq.back()]){
+//                 dq.pop_back();
+//             }
+//             dq.push_back(i);
+
+//             ans.push_back(nums[dq.front()]);
+//         }
+
+//         return ans;
