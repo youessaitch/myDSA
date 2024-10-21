4a33e7c41f5a15ec5b3003bf6c234815c31ef8f9
Ayush <72415411+youessaitch@users.noreply.github.com>
2024-10-21 22:48:17 +0530

    Time: 2106 ms (5.26%), Space: 108.3 MB (20.76%) - LeetHub

---
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int n;
+    int ans = 0;
+    vector<string> c;
+    void solve(int i, string &s, string temp,unordered_map<string,int> &mpp){
+        if(i>=n){
+            if(mpp.size()>ans){
+                ans = mpp.size();
+            }
+            return;
+        }
+
+        for(int j=i;j<n;j++){
+            temp += s[j];
+            // c.push_back(temp);
+            if(mpp.find(temp)==mpp.end()){
+                c.push_back(temp);
+                mpp[temp]++;
+                // cout<<"$$"<<" ";
+                solve(j+1,s,"",mpp);
+                mpp.erase(temp);
+                cout<<"$$"<<" ";
+                // c.pop_back();
+            }
+        }
+    }
+
+    int maxUniqueSplit(string s) {
+        n = s.size();
+        string temp = "";
+        unordered_map<string,int> mpp;
+        solve(0,s,temp,mpp);
+
+        for(auto it: c) cout<<it<<" ";
+        cout<<endl;
+
+        return ans;
+    }
+};
