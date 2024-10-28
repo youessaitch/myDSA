a520b2684a7bb129bec90f440318be255b513df2
Ayush <72415411+youessaitch@users.noreply.github.com>
2024-10-29 01:01:06 +0530

    0072-EDIT-DISTANCE Stats: Time: 8 ms (42.16%), Space: 13.1 MB (25.18%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,32 +1,48 @@
 class Solution {
 public:
-    int n, m;
+    int n,m;
     vector<vector<int>> dp;
 
-    int solve(int i, int j, string &word1, string &word2){
-        if(i>=n) return m-j;
-        if(j>=m) return n-i;
+    // int solve(int i, int j, string &word1, string &word2){
+    //     if(j>=m) return n-i;
+    //     if(i>=n) return m-j;
 
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        int take = 0;
-        int notTake = 0;
-        if(word1[i]==word2[j]){
-            take = solve(i+1,j+1,word1,word2);
-        }else{
-            int insertOp = 1 + solve(i,j+1, word1, word2);
-            int deleteOp = 1 + solve(i+1,j, word1, word2); 
-            int replaceOp = 1 + solve(i+1,j+1, word1, word2); 
+    //     if(dp[i][j]!=-1) return dp[i][j];
 
-            notTake = min(insertOp,min(deleteOp,replaceOp));
-        }
-        return dp[i][j] = take + notTake;
-    }
+    //     int take = 0;
+    //     if(word1[i]==word2[j]){
+    //         take = solve(i+1,j+1,word1,word2);
+    //     }else{
+    //         int ins = 1 + solve(i,j+1,word1,word2);
+    //         int del = 1 + solve(i+1,j,word1,word2);
+    //         int rep = 1 + solve(i+1,j+1,word1,word2);
+
+    //         take = min({ins,del,rep});
+    //     }
+
+    //     return dp[i][j] = take;
+    // }
 
     int minDistance(string word1, string word2) {
         n = word1.size();
         m = word2.size();
-        dp.resize(n+1,vector<int>(m+1,-1));
-        return solve(0,0,word1,word2);
+        // dp.resize(n+1,vector<int>(m+1,-1));
+        // return solve(0,0,word1,word2);
+        dp.resize(n+1,vector<int>(m+1,0));
+
+        for(int i=0;i<=n;i++) dp[i][0] = i;
+        for(int j=0;j<=m;j++) dp[0][j] = j;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1] != word2[j-1]){
+                    dp[i][j] = min({1+dp[i-1][j],1+dp[i][j-1],1+dp[i-1][j-1]});
+                }else{
+                    dp[i][j] = dp[i-1][j-1];
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
