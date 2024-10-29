87acb98a4daacec95b44124682fad49bcdc317d0
Ayush <72415411+youessaitch@users.noreply.github.com>
2024-10-30 03:23:40 +0530

    0801-IS-GRAPH-BIPARTITE Stats: Time: 0 ms (100%), Space: 17.1 MB (16.95%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>> &adj, vector<int> &color, int col, vector<int> &vis){
+    int n;
+    bool dfs(int node, int adj[][100], int color[], int col, int vis[]){
         vis[node] = 1;
         color[node] = col;
 
-        for(auto &it: adj[node]){
-            if(color[it] == col) return false;
-            if(!vis[it]){
-                if(dfs(it,adj,color,!col,vis)==false) return false;
+        for(int it=0;it<n;it++){
+            if(adj[node][it] == 1){
+                if(color[it] == col) return false;
+                if(!vis[it]){
+                    if(dfs(it,adj,color,!col,vis)==false) return false;
+                }
             }
         }
+
         return true;
     }
 
     bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<vector<int>> adj(n);
+        n = graph.size();
+        int adj[100][100] = {0};
         for(int i=0;i<n;i++){
             for(auto it: graph[i]){
-                adj[i].push_back(it);
-                adj[it].push_back(i);
+                adj[i][it] = 1;
+                adj[it][i] = 1;
             }
         }
 
-        vector<int> color(n,-1);
+        int color[100];
+        std::fill(color, color + n, -1); 
         int col = 1;
-        vector<int> vis(n,0);
+        int vis[100] = {0};
 
         for(int i=0;i<n;i++){
             if(!vis[i] && dfs(i,adj,color,col,vis)==false) return false;
