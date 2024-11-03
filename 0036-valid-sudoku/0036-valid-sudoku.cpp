f691af5e1e7f027fe4216a7830f447bb20e6c918
Ayush <72415411+youessaitch@users.noreply.github.com>
2024-11-03 17:59:09 +0530

    0036-VALID-SUDOKU Stats: Time: 1 ms (72.59%), Space: 24.7 MB (43.01%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        int n = 9;
-
-        for (int i = 0; i < 9; i++) {
-            unordered_map<char, int> hori;
-            unordered_map<char, int> vert;
-
-            for (int j = 0; j < 9; j++) {
-                // Check row
-                if (board[i][j] != '.') {
-                    if (hori.find(board[i][j]) != hori.end()) return false;
-                    else hori[board[i][j]]++;
-                }
-
-                // Check column
-                if (board[j][i] != '.') {
-                    if (vert.find(board[j][i]) != vert.end()) return false;
-                    else vert[board[j][i]]++;
-                }
-            }
-        }
-
-        // Check 3x3 sub-boxes
-        unordered_map<int,unordered_map<char,int>> box;
+    int n = 9;
+    bool isValidSudoku(vector<vector<char>>& grid) {
 
         for(int i=0;i<9;i++){
+            unordered_map<char,int> row;
+            unordered_map<char,int> col;
             for(int j=0;j<9;j++){
-                if(board[i][j]=='.') continue;
-                int boxIdx = 3*(i/3) + (j/3);
-                if (box[boxIdx].find(board[i][j]) != box[boxIdx].end()) return false;
-                else box[boxIdx][board[i][j]]++;
+                if(grid[i][j] != '.'){
+                    if(row.find(grid[i][j])!=row.end()) return false;
+                    else row[grid[i][j]]++;
+                }
+
+                if(grid[j][i] != '.'){
+                    if(col.find(grid[j][i])!=col.end()) return false;
+                    else col[grid[j][i]]++;
+                }
             }
         }
 
+        unordered_map<int,unordered_map<int,int>> box;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(grid[i][j]!='.'){
+                    int boxInd = 3*(i/3) + j/3;
+                    if(box[boxInd].find(grid[i][j])!=box[boxInd].end()) return false;
+                    else box[boxInd][grid[i][j]]++;
+                }
+            }
+        }
         return true;
     }
 };
