1493038ad3689992f8b71c939d68bd78b36bf72f
Ayush <72415411+youessaitch@users.noreply.github.com>
2024-12-02 03:03:29 +0530

    0152-MAXIMUM-PRODUCT-SUBARRAY Stats: Time: 0 ms (100%), Space: 17.8 MB (5.82%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    //Kadane's
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
-        int p = 1;
-        int s = 1;
-
-        int maxi = INT_MIN;
-
-        //left to right
+        int maxi = -1e9;
+        int curr = 1;
         for(int i=0;i<n;i++){
-            p*=nums[i];
-            maxi = max(maxi,p);
-            if(p==0) p=1;
+            curr*=nums[i];
+            maxi = max(maxi,curr);
+            if(curr == 0) curr = 1;
         }
 
-        //right to left
+        curr = 1;
         for(int i=n-1;i>=0;i--){
-            s*=nums[i];
-            maxi = max(maxi,s);
-            if(s==0) s=1;
+            curr*=nums[i];
+            maxi = max(maxi,curr);
+            if(curr == 0) curr = 1;
         }
-
         return maxi;
     }
 };
